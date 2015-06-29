49ad5ed9a8cfbb92763a955cf8aed2eafd6c9368
Rei <devel@reixd.net>
2015-06-29 12:45:30 +0200

    Add example module to show how to implement the BT service.

---
+++ Bluetooth/BluetoothService_sample/src/main/java/net/reixd/bluetoothservice_sample/MainActivity.java
@@ -0,0 +1,27 @@
+package net.reixd.bluetoothservice_sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button_start_bt_service);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(v.getContext(), MyService.class);
+                startService(serviceIntent);
+            }
+        });
+
+    }
+}
